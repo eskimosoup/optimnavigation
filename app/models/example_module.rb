4f8f1bb1fc508a5687fb0fd5352fd8a104b5d913
Optimised server <support@eskimosoup.co.uk>
2015-06-04 15:52:24 +0100

    Remove example module

--- app/models/example_module.rb
+++
@@ -1,2 +0,0 @@
-class ExampleModule < ActiveRecord::Base
-end
--- db/schema.rb
+++ db/schema.rb
@@ -12,15 +12,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20150604142903) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "example_modules", force: :cascade do |t|
-    t.string   "title"
-    t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "menu_item_hierarchies", id: false, force: :cascade do |t|
     t.integer "ancestor_id",   null: false
