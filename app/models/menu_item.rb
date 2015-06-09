2a0539830aa1e0d783bc611366f9b50391e7a2b7
Optimised server <support@eskimosoup.co.uk>
2015-06-09 13:52:37 +0100

    Layout and structure of menus and menu items

--- app/models/menu_item.rb
+++
@@ -1,7 +0,0 @@
-class MenuItem < ActiveRecord::Base
-  has_closure_tree order: :position
-
-  validates :menu_name, presence: true, length: { maximum: 100 }
-  validates :name, presence: true, length: { maximum: 100 }
-  validates :title_attribute, length: { maximum: 100 }
-end
